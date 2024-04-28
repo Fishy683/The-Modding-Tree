094b833c05aac2ba0ac19df4c7da2000fd052f2b
Fishy683 <phonedj63@gmail.com>
2024-04-28 11:52:55 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -40,7 +40,7 @@ function canGenPoints(){
 // Calculate points/sec!
 function getPointGen() {
 	if(!canGenPoints())
-		return new Decimal(1)
+		return new Decimal(2)
 
 	let gain = new Decimal(0)
 	return gain
