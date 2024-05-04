25ff4963546437a1eb5f8d4668616d1626cf3836
Fishy683 <phonedj63@gmail.com>
2024-05-04 22:49:41 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -40,7 +40,7 @@ function canGenPoints(){
 // Calculate points/sec!
 function getPointGen () {
 	if(!canGenPoints())
-		return new Decimal(0)
+		return new Decimal(1)
 	
 	let gain = new Decimal(0)
 	if(hasUpgrade('Planet',11)) gain = gain.add(1)
