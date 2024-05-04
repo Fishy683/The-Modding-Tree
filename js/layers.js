b2c0ea541094efc880858172c0d6d405cba226c3
Fishy683 <phonedj63@gmail.com>
2024-05-05 00:36:05 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -68,9 +68,39 @@ addLayer("Planet", {
         },
         24:{
             title: "Energy is near",
-            description: "Life gain is STRONG",
+            description: "Life gain is STRONGER",
             cost: new Decimal (1000),
         },
      },
+
+})
+
+addLayer("Star", {
+    name: "Star", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#CB2C14",
+    requires: new Decimal(5000), // Can be a function that takes requirement increases into account
+    resource: "Stars", // Name of prestige currency
+    baseResource: "Planets", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "s", description: "S: Reset for Star(s)", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
 
 })
